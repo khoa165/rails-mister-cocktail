c5ed25c4b1f7601a490e85d7b779e264a1988d22
Khoa Le <khoatle165@gmail.com>
2019-08-08 15:16:58 +0200

    reset db, fix frontend

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,33 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail
+  before_action :find_dose, only: [:destroy]
+
   def new
     @dose = Dose.new
   end
 
   def create
-    @cocktail = Cocktail.create(cocktail_strong_params)
-    if @cocktail.save #return true or false
-      redirect_to cocktail_path(@cocktail)
+    @dose = Dose.new(dose_strong_params)
+    @dose.cocktail =  @cocktail
+    if @dose.save #return true or false
+      redirect_to cocktail_path(@dose.cocktail)
     else
       render :new
     end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def find_dose
+    @dose = Dose.find(params[:id])
   end
 end
