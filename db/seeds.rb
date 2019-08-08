5fb9113b2cc2aa81838d0f964a3b0f249e81d000
Khoa Le <khoatle165@gmail.com>
2019-08-08 12:26:46 +0200

    add ingredients in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json_file = open(url).read
+result = JSON.parse(json_file)
+
+drinks = result["drinks"]
+drinks.each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
+
+puts 'Finished!'
