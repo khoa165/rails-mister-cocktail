d824579405227f36d86d5a4b2e0d42d1ae9b6705
Khoa Le <khoatle165@gmail.com>
2019-08-08 14:54:45 +0200

    work on controller and views for dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,14 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_strong_params)
+    if @cocktail.save #return true or false
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
 end
