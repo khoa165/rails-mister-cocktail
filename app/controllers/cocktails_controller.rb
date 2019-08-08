0fa134bb97487bd71b3e37aeb45ebd1980fd3ed2
Khoa Le <khoatle165@gmail.com>
2019-08-08 12:50:18 +0200

    work on cocktails controller and pages

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,33 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_strong_params)
+    if @cocktail.save #return true or false
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_strong_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
